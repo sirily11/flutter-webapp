1106e76ff1ee584cee3ad56635372a992bd0c548
sirily11 <sirily11@users.noreply.github.com>
2020-08-03 14:31:09 +0000

    deploy: 504301486c6913f535697e8a2548ce49c3a28088

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,14 +3,14 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "main.dart.js": "f66906d4b7774ce9ab68ce3f61a591c9",
+  "main.dart.js": "a0f295ca1bbd26721b2bcaeb19fb1d4c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "index.html": "3ed2d5b6cf6c6c70aab23df5d0cbc455",
 "/": "3ed2d5b6cf6c6c70aab23df5d0cbc455",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
 "assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
-"assets/NOTICES": "5747af59e6892ab076fd1a6f43eda660",
+"assets/NOTICES": "a875403ea765c87dadee0ce9650c706b",
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "manifest.json": "ce5e5d399970e4423ded5ceeecfb8ae2",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
